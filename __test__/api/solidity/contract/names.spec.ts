068d41daed75226ddfd5c9f9184f0400c7207036
sirily11 <sirily1997@gmail.com>
2022-11-10 18:17:20 +0800

    feat: add names api

---
+++ __test__/api/solidity/contract/names.spec.ts
@@ -0,0 +1,28 @@
+import { service } from "../../../../pages/api/solidity/contract/names";
+
+describe("Given a naming service", () => {
+  it("should return a list of names", () => {
+    const source = `
+        contract Foo {
+            uint256 bar;
+        }
+        `;
+    const response = service({ source });
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.names).toEqual(["Foo"]);
+  });
+
+  it("should return a list of names", () => {
+    const source = `
+        contract Foo {
+            uint256 bar;
+        }
+        contract Bar {
+            uint256 foo;
+        }
+        `;
+    const response = service({ source });
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.names).toEqual(["Foo", "Bar"]);
+  });
+});
